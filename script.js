0f127f3acf824ef1d158923c734cde7e0fda756e
Praveen Racharla <praveen.racharla@statnett.no>
2026-06-26 15:45:43 +0200

    altering code to just include pie charts

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-// JavaScript file
+const API_URL = "https://YOUR-RENDER-APP.onrender.com";
+
 async function loadExpenses() {
-  const res = await fetch("http://127.0.0.1:5000/expenses");
+  const res = await fetch(`${API_URL}/expenses`);
   const data = await res.json();
 
   console.log(data);
 
-  // TOTAL
   const total = data.reduce((sum, e) => sum + e.amount, 0);
   document.getElementById("total").innerText = total + " NOK";
 
-  // GROUP BY CATEGORY
   const map = {};
   data.forEach(e => {
     map[e.category] = (map[e.category] || 0) + e.amount;
   });
 
-  // PIE CHART
   new Chart(document.getElementById("pieChart"), {
     type: "pie",
     data: {
@@ -25,46 +23,15 @@ async function loadExpenses() {
       }]
     }
   });
-}
 
-async function addExpense() {
-  const date = document.getElementById("date").value;
-  const category = document.getElementById("category").value;
-  const amount = document.getElementById("amount").value;
-  const note = document.getElementById("note").value;
+  const list = document.getElementById("list");
+  list.innerHTML = "";
 
-  if (!date || !category || !amount) {
-    alert("Please fill required fields");
-    return;
-  }
-
-  const res = await fetch(API_URL + "/expenses", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      date,
-      category,
-      amount,
-      note
-    })
+  Object.entries(map).forEach(([cat, val]) => {
+  const li = document.createElement("li");
+  li.innerText = `${cat}: ${val} NOK`;
+  list.appendChild(li);
   });
-
-  if (res.ok) {
-    alert("Expense added!");
-
-    // refresh dashboard
-    loadExpenses();
-
-    // clear form
-    document.getElementById("date").value = "";
-    document.getElementById("category").value = "";
-    document.getElementById("amount").value = "";
-    document.getElementById("note").value = "";
-  } else {
-    alert("Error adding expense");
-  }
 }
 
 loadExpenses();
