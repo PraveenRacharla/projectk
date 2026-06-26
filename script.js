32e207c58ed6190a47d46c832d3d677560c6ec5d
Praveen Racharla <praveen.racharla@statnett.no>
2026-06-26 13:25:53 +0200

    jsscript test

---
+++ script.js
@@ -0,0 +1,30 @@
+// JavaScript file
+async function loadExpenses() {
+  const res = await fetch("http://127.0.0.1:5000/expenses");
+  const data = await res.json();
+
+  console.log(data);
+
+  // TOTAL
+  const total = data.reduce((sum, e) => sum + e.amount, 0);
+  document.getElementById("total").innerText = total + " NOK";
+
+  // GROUP BY CATEGORY
+  const map = {};
+  data.forEach(e => {
+    map[e.category] = (map[e.category] || 0) + e.amount;
+  });
+
+  // PIE CHART
+  new Chart(document.getElementById("pieChart"), {
+    type: "pie",
+    data: {
+      labels: Object.keys(map),
+      datasets: [{
+        data: Object.values(map)
+      }]
+    }
+  });
+}
+
+loadExpenses();
